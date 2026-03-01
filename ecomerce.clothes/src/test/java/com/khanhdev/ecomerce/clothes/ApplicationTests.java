99f04c9c46e1e0e73f462ecb8b94f422c128954e
KhanhNguyen <khanh.nguyenphanhuu@gmail.com>
2026-03-01 21:02:12 +0700

    init project

---
+++ ecomerce.clothes/src/test/java/com/khanhdev/ecomerce/clothes/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khanhdev.ecomerce.clothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
