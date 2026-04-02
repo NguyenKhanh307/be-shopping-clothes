98758f231d955897b9a91b6aa6843163ac9d8eef
Huyền Trân <shuyentran2205@gmail.com>
2026-04-02 22:20:48 +0700

    hoàn thành các API register, login, resetpassword

---
+++ src/main/java/com/khanhdev/be_shopping_clothes/dto/RegisterRequest.java
@@ -0,0 +1,11 @@
+package com.khanhdev.be_shopping_clothes.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+}
--- src/main/java/com/khanhdev/be_shopping_clothes/repository/UserRepository.java
+++ src/main/java/com/khanhdev/be_shopping_clothes/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.khanhdev.be_shopping_clothes.repository;
 import com.khanhdev.be_shopping_clothes.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Integer> {
-    
+    Optional<User> findByEmail(String email);
 }
